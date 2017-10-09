782f04cadc84af721370287a429dd54e067d301c
Sjonneis <jeroenbiggie@gmail.com>
2017-10-09 10:02:53 +0200

    Gewoon boer man
    Niet omdat het moet, maar omdat het kan.

--- includes/home.php
+++ includes/home.php
@@ -1,6 +1,6 @@
 <?php
-include('head.inc.php');
-include('header.inc.php');
-include('homebody.inc.php');
-include('footer.inc.php');
+//include('head.inc.php');
+//include('header.inc.php');
+//include('homebody.inc.php');
+//include('footer.inc.php');
 ?>
